84fafd35ad9ab62471f3df6f1c623d22898d528d
roman-mironov <roman-mironov@users.noreply.github.com>
2020-04-28 17:01:13 +0100

    Update cyberInky.py

--- cyberInky.py
+++ cyberInky.py
@@ -1,4 +1,4 @@
-#CyberInky Code by Roman Mironov
+CyberInky Code by Roman Mironov
 
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
@@ -6,7 +6,7 @@ from datetime import *
 
 #Calculate release date
 today = date.today()
-release_date = date(2020,04,16)
+release_date = date(2020,9,17)
 diff = release_date - today
 
 #Configure InkyPHAT
@@ -14,18 +14,14 @@ inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.BLACK)
 
 # Load graphic
-img = Image.open("/home/pi/Projects/cyberpunk_red3.png")
-#inky_display.set_image(img)
-#inky_display.show()
+img = Image.open("/home/pi/Downloads/cyberInky/cyberpunk_red3.png")
 
 #Print text
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("/home/pi/Projects/hacker.otf", 18)
+font = ImageFont.truetype("/home/pi/Downloads/cyberInky/hacker.otf", 18)
 
 message= ">Days left: "+str(diff.days)
 w, h = font.getsize(message)
-#x = (inky_display.WIDTH / 2) - (w / 2)
-#y = (inky_display.HEIGHT / 2) - (h / 2)
 x = 10
 y = 80
 
